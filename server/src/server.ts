efc573b874aa28d9139ce62c68805a05f1aa5321
VanceGoblin <zvkubajak@gmail.com>
2024-09-21 18:26:56 -0500

    Updated server.ts to Support stockRoutes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,14 @@
 import express from "express";
 import cors from "cors";
+import stockRoutes from "./routes/stockRoutes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api", stockRoutes);
 
 const PORT = 3001;
-
-app.get("/", (_req, res) => {
-  res.send("Server is running.");
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
