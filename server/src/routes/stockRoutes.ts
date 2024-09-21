1c398b2157b3fe400e15d8f6960c5869fb1b027f
VanceGoblin <zvkubajak@gmail.com>
2024-09-21 18:23:16 -0500

    Created stockRoutes.ts that gets a ticker and calls fetchStockData using the ticker as the parameter

---
+++ server/src/routes/stockRoutes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { fetchStockData } from "../controllers/stockController";
+
+const router = Router();
+
+router.get("/stocks/:ticker", fetchStockData);
+
+export default router;
