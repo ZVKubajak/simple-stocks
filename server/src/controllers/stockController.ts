7fe7f7e49326444feb457bad9fb8e6a08cb65124
VanceGoblin <zvkubajak@gmail.com>
2024-09-21 19:41:23 -0500

    Updated stockController.ts with Interfaces

--- server/src/controllers/stockController.ts
+++ server/src/controllers/stockController.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from "express";
 import { getStockData } from "../services/polygonService";
+import { StockData } from "../types/stockData";
 
-export const fetchStockData = async (req: Request, res: Response) => {
+export const fetchStockData = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   const { ticker } = req.params;
 
   try {
-    const stockData = await getStockData(ticker);
+    const stockData: StockData = await getStockData(ticker);
     res.json(stockData);
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch stock data." });
