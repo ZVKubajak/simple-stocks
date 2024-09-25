4abf3653687a42526b30df9f6bafbf68f37f423b
VanceGoblin <zvkubajak@gmail.com>
2024-09-25 16:58:25 -0500

    Created stockController.ts (has parameter errors

--- server/src/controllers/stockController.ts
+++ server/src/controllers/stockController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { getCurrentStockData } from "../services/stocks/currentSService";
+import { getHistoricalStockData } from "../services/stocks/historicalSService";
 import { StockData } from "../types/stockData";
 
 export const fetchCurrentStockData = async (
@@ -12,6 +13,26 @@ export const fetchCurrentStockData = async (
     const currentStockData: StockData = await getCurrentStockData(ticker);
     res.json(currentStockData);
   } catch (error) {
-    res.status(500).json({ error: "Failed to fetch stock data." });
+    res.status(500).json({ error: "Failed to fetch current stock data." });
+  }
+};
+
+export const fetchHistoricalStockData = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { ticker } = req.params;
+  const { multiplier, from, to } = req.query;
+
+  try {
+    const historicalStockData: StockData = await getHistoricalStockData(
+      ticker,
+      Number(multiplier),
+      from as string,
+      to as string
+    );
+    res.json(historicalStockData);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch historical stock data." });
   }
 };
