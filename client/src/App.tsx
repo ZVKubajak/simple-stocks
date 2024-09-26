c9141fde94275e93ce80b569e5217131c8b2091f
VanceGoblin <zvkubajak@gmail.com>
2024-09-26 16:07:30 -0500

    Added React Routers to main.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
     <>
 
     </>
-  )
+  );
 }
 
 export default App
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,25 @@
-import { StrictMode } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+import App from "./App.tsx";
+import Stocks from "./pages/Stocks.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Stocks />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <RouterProvider router={router} />
 );
