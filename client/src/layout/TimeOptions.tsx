80a0794333a653e4157fe558dcc7eeac6a1986fc
VanceGoblin <zvkubajak@gmail.com>
2024-10-07 15:20:12 -0500

    Removed handleSelection and Now Using React Bootstrap Radio Buttons

--- client/src/layout/TimeOptions.tsx
+++ client/src/layout/TimeOptions.tsx
@@ -3,12 +3,34 @@ import ButtonGroup from "react-bootstrap/ButtonGroup";
 import ToggleButton from "react-bootstrap/ToggleButton";
 
 const TimePeriodOptions = () => {
-  const [selectedPeriod, setSelectedPeriod] = useState("1Y");
+  const [periodValue, setPeriodValue] = useState("1");
 
-  const handleSelection = (timePeriod: string) => {
-    setSelectedPeriod(timePeriod);
-    console.log("Selectd Time Period:", timePeriod);
-  };
+  const timePeriods = [
+    { name: "1M", value: "1" },
+    { name: "6M", value: "2" },
+    { name: "YTD", value: "3" },
+    { name: "1Y", value: "4" },
+    { name: "2Y", value: "5" },
+  ];
 
-  return <div></div>;
+  return (
+    <ButtonGroup>
+      {timePeriods.map((timePeriod, idx) => (
+        <ToggleButton
+          key={idx}
+          id={`timePeriod-${idx}`}
+          type="radio"
+          variant={"outline-success"}
+          name="timePeriod"
+          value={timePeriod.value}
+          checked={periodValue === timePeriod.value}
+          onChange={(e) => setPeriodValue(e.currentTarget.value)}
+        >
+          {timePeriod.name}
+        </ToggleButton>
+      ))}
+    </ButtonGroup>
+  );
 };
+
+export default TimePeriodOptions;
