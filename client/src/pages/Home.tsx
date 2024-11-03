8448e33117e610997dc61e274169b71f00a7403c
VanceGoblin <zvkubajak@gmail.com>
2024-11-03 16:35:13 -0600

    Created Layout For Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,17 @@
+import Button from "react-bootstrap/Button";
+
 const Home = () => {
   return (
     <main>
+      <h1>Simple Stocks</h1>
+      <h2>A Beginner's Guide To The Stock Market</h2>
 
+      <p>
+        Our app aims to give users a straightforward way to learn about stocks by providing a manageable system to view the market.
+      </p>
+      <p>Get started now and make smart decisions!</p>
+
+      <Button variant="success">Search For A Stock</Button>
     </main>
   );
 };
