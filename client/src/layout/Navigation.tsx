2593fad4e013cd0f80ec1663cfb3944f550a583c
VanceGoblin <zvkubajak@gmail.com>
2024-11-03 16:01:10 -0600

    Set Up Navigation.tsx With React Routers

--- client/src/layout/Navigation.tsx
+++ client/src/layout/Navigation.tsx
@@ -3,16 +3,25 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
 import "../assets/css/css-layout/Navigation.css";
+import { Link, useLocation } from "react-router-dom";
 
 const Navigation = () => {
+  const currentPage = useLocation().pathname;
+
   return (
     <Navbar bg="dark" data-bs-theme="dark" id="navbar">
       <Container>
-        <Navbar.Brand href="#home">Simple Stocks</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">Simple Stocks</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#explore">Explore</Nav.Link>
-          <Nav.Link href="#contact">Contact</Nav.Link>
+          <Nav.Link as={Link} to="/" active={currentPage === "/"}>
+            Home
+          </Nav.Link>
+          <Nav.Link as={Link} to="/stocks" active={currentPage === "/stocks"}>
+            Explore
+          </Nav.Link>
+          <Nav.Link as={Link} to="/contact" active={currentPage === "/contact"}>
+            Contact
+          </Nav.Link>
         </Nav>
       </Container>
     </Navbar>
