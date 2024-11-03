9107b8f08fc3bd0d1e888657cb846ad29c1b8160
VanceGoblin <zvkubajak@gmail.com>
2024-11-03 15:37:50 -0600

    Created Home & Contact Pages and Set Up main.tsx Paths

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 import App from "./App.tsx";
+import Home from "./pages/Home.tsx";
 import Stocks from "./pages/Stocks.tsx";
+import Contact from "./pages/Contact.tsx";
 
 const router = createBrowserRouter([
   {
@@ -14,7 +16,15 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
+        element: <Home />,
+      },
+      {
+        path: "/stocks",
         element: <Stocks />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
       },
     ],
   },
